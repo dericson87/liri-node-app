b24059a6cb922ddf8397c7771299e311964a49ef
Derek Ericson <derek.ericson@gmail.com>
2019-02-25 18:25:14 -0800

    Update liri.js

--- liri.js
+++ liri.js
@@ -1,3 +1,5 @@
+//COMMANDS: concert-this, spotify-this-song, movie-this, do-what-it-says
+
 require("dotenv").config();
 
 var request = require("request");
@@ -5,6 +7,8 @@ var fs = require("fs");
 var keys = require("./keys.js");
 var Spotify = require('node-spotify-api');
 var spotify = new Spotify(keys.spotify);
+
+let chalk = require('chalk');
 
 var userOption = process.argv[2]; 
 var inputParameter = process.argv[3];
@@ -17,7 +21,7 @@ function UserInputs (userOption, inputParameter){
         showConcertInfo(inputParameter);
         break;
     case 'spotify-this-song':
-        showSongInfo(inputParameter);
+        spotifySong(inputParameter);
         break;
     case 'movie-this':
         showMovieInfo(inputParameter);
@@ -26,7 +30,7 @@ function UserInputs (userOption, inputParameter){
         showSomeInfo();
         break;
     default: 
-        console.log("Valid commands: \nconcert-this <'name of band'> \nspotify-this-song <'name of song'>\nmovie-this <'name of movie'>\ndo-what-it-says")
+        console.log("Valid commands: \nconcert-this <'name of band'> \nspotify-this-song <'name of song'>\nmovie-this <'name of movie'>\ndo-what-it-says");
     }
 }
 
@@ -36,76 +40,72 @@ function showConcertInfo(inputParameter){
     request(queryUrl, function(error, response, body) {
     if (!error && response.statusCode === 200) {
         var concerts = JSON.parse(body);
-        for (var i = 0; i < concerts.length; i++) {  
-            console.log("**********EVENT INFO*********");  
-            fs.appendFileSync("log.txt", "**********EVENT INFO*********\n");
-            console.log(i);
+        for (var i = 0; i < concerts.length; i++) {    
+          display(chalk.blue("\n********EVENT INFO*********\n"));  
+            fs.appendFileSync("log.txt", "\n********EVENT INFO***********\n");
+           display(chalk.green(i));
             fs.appendFileSync("log.txt", i+"\n");
-            console.log("Name of the venue: " + concerts[i].venue.name);
+           display(chalk.green("Name of the venue: " + concerts[i].venue.name));
             fs.appendFileSync("log.txt", "Name of the venue: " + concerts[i].venue.name+"\n");
-            console.log("Venue location: " +  concerts[i].venue.city);
+           display(chalk.green("Venue location: " +  concerts[i].venue.city));
             fs.appendFileSync("log.txt", "Venue location: " +  concerts[i].venue.city+"\n");
             var dTime = concerts[i].datetime;
             var month = dTime.substring(5,7);
             var year = dTime.substring(0,4);
             var day = dTime.substring(8,10);
             var dateForm = month + "/" + day + "/" + year
-            console.log("Date of the Event: " +  dateForm);
+           display(chalk.green("Date of the Event: " +  dateForm));
             fs.appendFileSync("log.txt", "Date of the Event: " + dateForm +"\n");
-            console.log("*****************************");
-            fs.appendFileSync("log.txt", "*****************************"+"\n");
+           display(chalk.blue("\n***************************"));
+            fs.appendFileSync("log.txt", "******************************");
         }
     } else{
-      console.log('Error occurred.');
+      display(chalk.red('Error occurred.'));
     }
 });}
 
 //Spotify
-function showSongInfo(inputParameter) {
-    if (inputParameter === undefined) {
-        inputParameter = "The Sign"; //default Song
-    }
-    spotify.search(
-        {
-            type: "track",
-            query: inputParameter
-        },
-        function (err, data) {
-            if (err) {
-                console.log("Error occurred: " + err);
-                return;
-            }
-            var songs = data.tracks.items;
+function spotifySong(parameter) {
+  var searchTrack;
+  if (parameter === undefined) {
+    searchTrack = "Ace of Base The Sign";
+  } else {
+    searchTrack = parameter;
+  }
 
-            for (var i = 0; i < songs.length; i++) {
-                console.log("**********SONG INFO*********");
-                fs.appendFileSync("log.txt", "**********SONG INFO*********\n");
-                console.log(i);
-                fs.appendFileSync("log.txt", i +"\n");
-                console.log("Song name: " + songs[i].name);
-                fs.appendFileSync("log.txt", "song name: " + songs[i].name +"\n");
-                console.log("Preview song: " + songs[i].preview_url);
-                fs.appendFileSync("log.txt", "preview song: " + songs[i].preview_url +"\n");
-                console.log("Album: " + songs[i].album.name);
-                fs.appendFileSync("log.txt", "album: " + songs[i].album.name + "\n");
-                console.log("Artist(s): " + songs[i].artists[0].name);
-                fs.appendFileSync("log.txt", "artist(s): " + songs[i].artists[0].name + "\n");
-                console.log("*****************************");  
-                fs.appendFileSync("log.txt", "*****************************\n");
-             }
-        }
-    );
+  spotify.search({
+    type: 'track',
+    query: searchTrack
+  }, function(error, data) {
+    if (error) {
+      display('Error recorded: ' + error);
+      return;
+    } else {
+      display(chalk.blue("\n**********SONG INFO***********\n"));
+      fs.appendFileSync("log.txt","\n**********SONG INFO***********\n");
+      display(chalk.green("Artist: " + data.tracks.items[0].artists[0].name));
+      fs.appendFileSync("log.txt","Artist: " + data.tracks.items[0].artists[0].name + "\n");
+      display(chalk.green("Song: " + data.tracks.items[0].name));
+      fs.appendFileSync("log.txt","Song: " + data.tracks.items[0].name + "\n");
+      display(chalk.green("Preview: " + data.tracks.items[3].preview_url));
+      fs.appendFileSync("log.txt","Preview: " + data.tracks.items[3].preview_url + "\n");
+      display(chalk.green("Album: " + data.tracks.items[0].album.name));
+      fs.appendFileSync("log.txt","Album: " + data.tracks.items[0].album.name);
+      display(chalk.blue("\n**********SONG INFO***********\n"));
+      fs.appendFileSync("log.txt", "\n**********SONG INFO***********\n");  
+    }
+  });
 };
 
 //OMDB
 function showMovieInfo(inputParameter){
     if (inputParameter === undefined) {
         inputParameter = "Mr. Nobody"
-        console.log("-----------------------");
+        display(chalk.red("-----------------------"));
         fs.appendFileSync("log.txt", "-----------------------\n");
-        console.log("If you haven't watched 'Mr. Nobody', then you should: http://www.imdb.com/title/tt0485947/");
+        display(chalk.red("If you haven't watched 'Mr. Nobody', then you should: http://www.imdb.com/title/tt0485947/"));
         fs.appendFileSync("log.txt", "If you haven't watched 'Mr. Nobody', then you should: http://www.imdb.com/title/tt0485947/" +"\n");
-        console.log("It's on Netflix!");
+        display(chalk.red("It's on Netflix!"));
         fs.appendFileSync("log.txt", "It's on Netflix!\n");
     }
     var queryUrl = "http://www.omdbapi.com/?t=" + inputParameter + "&y=&plot=short&apikey=trilogy";
@@ -113,25 +113,25 @@ function showMovieInfo(inputParameter){
    
     if (!error && response.statusCode === 200) {
         var movies = JSON.parse(body);
-        console.log("**********MOVIE INFO*********");  
+        display(chalk.blue("\n**********MOVIE INFO*********\n"));  
         fs.appendFileSync("log.txt", "**********MOVIE INFO*********\n");
-        console.log("Title: " + movies.Title);
+        display(chalk.green("Title: " + movies.Title));
         fs.appendFileSync("log.txt", "Title: " + movies.Title + "\n");
-        console.log("Release Year: " + movies.Year);
+       display(chalk.green("Release Year: " + movies.Year));
         fs.appendFileSync("log.txt", "Release Year: " + movies.Year + "\n");
-        console.log("IMDB Rating: " + movies.imdbRating);
+        display(chalk.green("IMDB Rating: " + movies.imdbRating));
         fs.appendFileSync("log.txt", "IMDB Rating: " + movies.imdbRating + "\n");
-        console.log("Rotten Tomatoes Rating: " + getRottenTomatoesRatingValue(movies));
+       display(chalk.green("Rotten Tomatoes Rating: " + getRottenTomatoesRatingValue(movies)));
         fs.appendFileSync("log.txt", "Rotten Tomatoes Rating: " + getRottenTomatoesRatingValue(movies) + "\n");
-        console.log("Country of Production: " + movies.Country);
+       display(chalk.green("Country of Production: " + movies.Country));
         fs.appendFileSync("log.txt", "Country of Production: " + movies.Country + "\n");
-        console.log("Language: " + movies.Language);
+       display(chalk.green("Language: " + movies.Language));
         fs.appendFileSync("log.txt", "Language: " + movies.Language + "\n");
-        console.log("Plot: " + movies.Plot);
+       display(chalk.green("Plot: " + movies.Plot));
         fs.appendFileSync("log.txt", "Plot: " + movies.Plot + "\n");
-        console.log("Actors: " + movies.Actors);
+       display(chalk.green("Actors: " + movies.Actors));
         fs.appendFileSync("log.txt", "Actors: " + movies.Actors + "\n");
-        console.log("*****************************");  
+        display(chalk.blue("\n**********MOVIE INFO*********\n"));  
         fs.appendFileSync("log.txt", "*****************************\n");
     } else{
       console.log('Error occurred.');
@@ -157,5 +157,9 @@ function showSomeInfo(){
 		}
         var dataArr = data.split(',');
         UserInputs(dataArr[0], dataArr[1]);
-	});
+    });
 }
+    function display (dataToLog) {
+        console.log(dataToLog);
+    }
+    
